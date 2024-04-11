d76557956aeff6446a15acbb0cf8faf3bef15439
MarinaTalochka <153221203+MarinaTalochka@users.noreply.github.com>
2024-04-11 23:28:18 +0300

    Предэкзамен

--- script.js
+++ script.js
@@ -29,61 +29,60 @@ const words = [
         translation: 'Ежевика',
         example: "blackberry - a berry for everyone"
     }
-  ]
-  
-  const card = document.querySelector(".flip-card");
-  const prevButton = document.querySelector("#back");
-  const nextButton = document.querySelector("#next");
-  const testButton = document.querySelector("#exam");
-  const wordNumberElement = document.querySelector("#current-word");
-  let idx = 0;
-  
-  function prepareCard({ word, translation, example }) {
-  renderCard({ word, translation, example });
-  card.addEventListener("click", (event) => {
-    if (idx !== words.length - 1) {
-      event.currentTarget.classList.toggle("active");
-    }
-  });
-  }
-  
-  function renderCard({ word, translation, example }) {
-  card.querySelector("#card-front h1").textContent = word || "Default Word";
-  card.querySelector("#card-back h1").textContent = translation || "Default Translation";
-  card.querySelector("#card-back p span").textContent = example || "Default Example";
-  
-  }
-  
-  function updateButtons() {
-  prevButton.disabled = idx === 0;
-  nextButton.disabled = idx === words.length - 1;
-  }
-  function updateWordNumber() {
+]
+
+const card = document.querySelector(".flip-card");
+const prevButton = document.querySelector("#back");
+const nextButton = document.querySelector("#next");
+const testButton = document.querySelector("#exam");
+const wordNumberElement = document.querySelector("#current-word");
+let idx = 0;
+
+function prepareCard({ word, translation, example }) {
+    renderCard({ word, translation, example });
+    card.addEventListener("click", (event) => {
+        if (idx !== words.length - 1) {
+            event.currentTarget.classList.toggle("active");
+        }
+    });
+}
+
+function renderCard({ word, translation, example }) {
+    card.querySelector("#card-front h1").textContent = word || "Default Word";
+    card.querySelector("#card-back h1").textContent = translation || "Default Translation";
+    card.querySelector("#card-back p span").textContent = example || "Default Example";
+
+}
+
+function updateButtons() {
+    prevButton.disabled = idx === 0;
+    nextButton.disabled = idx === words.length - 1;
+}
+function updateWordNumber() {
     wordNumberElement.textContent = `${idx + 1} из ${words.length}`;
-  }
-  
-  // Добавьте обработчики событий для кнопок
-  prevButton.addEventListener("click", () => {
-  idx = Math.max(0, idx - 1);
-  prepareCard(words[idx]);
-  updateButtons();
-  });
-  
-  nextButton.addEventListener("click", () => {
-  idx = Math.min(words.length - 1, idx + 1);
-  prepareCard(words[idx]);
-  updateButtons();
-  });
-  
-  testButton.addEventListener("click", () => {
-  // Логика перехода в режим тестирования
-  // ...
-  });
-  
-  // Вызов функции для подготовки начальной карточки
-  prepareCard(words[idx]);
-  
-  updateButtons();
-  updateWordNumber()
-  
-  
+}
+
+// Добавьте обработчики событий для кнопок
+prevButton.addEventListener("click", () => {
+    idx = Math.max(0, idx - 1);
+    prepareCard(words[idx]);
+    updateButtons();
+});
+
+nextButton.addEventListener("click", () => {
+    idx = Math.min(words.length - 1, idx + 1);
+    prepareCard(words[idx]);
+    updateButtons();
+});
+
+testButton.addEventListener("click", () => {
+    // Логика перехода в режим тестирования
+    // ...
+});
+
+// Вызов функции для подготовки начальной карточки
+prepareCard(words[idx]);
+
+updateButtons();
+updateWordNumber()
+
