9003647e5f2dd34438f7352ecbf955fc4c4efa21
MarinaTalochka <153221203+MarinaTalochka@users.noreply.github.com>
2024-04-18 19:55:57 +0300

    Экзамен

--- assets/fbh.js
+++ assets/fbh.js
@@ -1,3 +1,4 @@
+
 const words = [
     {
         word: 'Apple',
@@ -37,10 +38,6 @@ const nextButton = document.querySelector("#next");
 const testButton = document.querySelector("#exam");
 const wordNumberElement = document.querySelector("#current-word");
 const totalWordsElement = document.querySelector("#total-word");
-const studyCards = document.querySelector(".study-cards");
-const examCards = document.querySelector("#exam-cards");
-const examModeProgress = document.querySelector(".sidebar #exam-mode");
-
 
 let idx = 0;
 
@@ -55,7 +52,7 @@ function prepareCard({ word, translation, example }) {
 
 function renderCard({ word, translation, example }) {
     card.querySelector("#card-front h1").textContent = word || "Default Word";
-    card.querySelector("#card-back h1").textContent = translation ||  "Default Translation";
+    card.querySelector("#card-back h1").textContent = translation || "Default Translation";
     card.querySelector("#card-back p span").textContent = example || "Default Example";
 
 }
@@ -93,18 +90,15 @@ const russianWords = words.map(function (item) {
 const allWords = [...englishWords, ...russianWords]; // формируем масив из англ и русских слов
 
 testButton.addEventListener('click', function () {
-    studyCards.classList.add('hidden');
 
     const fragment = new DocumentFragment();
     for (let i = 0; i < allWords.length; i++) {
         const element = document.createElement('span');
         element.classList.add('card');
-        element.dataset.index = i;
         element.textContent = allWords[i];
-        examCards.append(element);
+        totalWordsElement.append(element);
     };
-
-    examCards.append(fragment);
+    totalWordsElement.append(fragment);
 
 });
 
@@ -115,60 +109,41 @@ let firstCardIndex = null;
 function handleCardClick(event) {
    
     const clickedCard = event.target;
-    if (clickedCard.classList.contains('.fade-out') || !clickedCard.classList.contains('card')) {
-        return
-    }
     const clickedIndex = parseInt(clickedCard.dataset.index);
    
-   
-    if (firstCardIndex == null) {
+    if (firstCardIndex === null) {
         // Первая выбранная карточка
         firstCardIndex = clickedIndex;
         clickedCard.classList.add('correct');
-
     } else {
         // Вторая выбранная карточка
         const firstCard = document.querySelector(`[data-index='${firstCardIndex}']`);
-
-        const wordObject = words.find(word => word.word === firstCard.textContent || word.translation === firstCard.textContent);
-        
-        if(firstCard.textContent !== clickedCard.textContent) {
-            if (wordObject.translation === clickedCard.textContent || wordObject.word === clickedCard.textContent) {
-                // Пара подобрана верно
-                firstCard.classList.add('fade-out');
-                firstCard.style.pointerEvents = "none";
-                clickedCard.classList.add('correct', 'fade-out');
-                clickedCard.style.pointerEvents = "none";
-                firstCardIndex = null;
-                
-            } else {
-                // Пара подобрана неверно
-                clickedCard.classList.add('wrong');
-                setTimeout(() => {
-                    clickedCard.classList.remove('wrong');
-                    firstCard.classList.remove('correct');
-                    firstCardIndex = null;
-    
-                }, 500);
-            }
+        if (words[firstCardIndex] === words[clickedIndex]) {
+            // Пара подобрана верно
+            firstCard.classList.add('fade-out');
+            clickedCard.classList.add('fade-out');
+        } else {
+            // Пара подобрана неверно
+            clickedCard.classList.add('wrong');
+            setTimeout(() => {
+                clickedCard.classList.remove('wrong');
+            }, 500);
         }
-
+        firstCardIndex = null;
     }
 
     // Проверяем, остались ли еще видимые карточки
     if (document.querySelectorAll('.card:not(.fade-out)').length === 0) {
-        setTimeout(() => {
-            alert('Поздравляем! Вы успешно завершили проверку знаний.');
-        }, 700);
-        
+        alert('Поздравляем! Вы успешно завершили проверку знаний.');
     }
 }
 
 // Добавляем обработчик клика на контейнер с карточками
-examCards.addEventListener('click', handleCardClick);
+totalWordsElement.addEventListener('click', handleCardClick);
 
 
 prepareCard(words[idx]);
 
 updateButtons();
 updateWordNumber()
+
